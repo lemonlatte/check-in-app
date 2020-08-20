206d4eeef7fb320613a8986f1ccd71963a7dcdaf
Jim Yeh <jim@bitmark.com>
2020-08-20 20:59:48 +0800

    First commit

---
+++ src/components/Student.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="students container">
+    <div class="row row-cols-5" v-for="i in Math.ceil(numbers.length / 5)" v-bind:key="i">
+      <div class="col" v-for="n in numbers.slice((i - 1) * 5, i * 5)" v-bind:key="n">
+        <p>座號: {{n}}<br>
+        <button type="button" class="btn btn-success">簽到</button>
+        </p>
+        <p> </p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+  import {
+    Options,
+    Vue
+  } from 'vue-class-component';
+
+  @Options({
+    data() {
+      return {
+        numbers: Array(22).fill(0).map((v, i) => i + 1)
+      }
+    }
+  })
+  export default class Student extends Vue {
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="time">
+    <h1>706 每日簽到表</h1>
+    <h2>現在時間：{{ diplayTime }}</h2>
+  </div>
+  <div class="home">
+    <Student />
+  </div>
+</template>
+
+<script lang="ts">
+  import {
+    Options,
+    Vue
+  } from 'vue-class-component';
+  import moment from 'moment'
+  import Student from '@/components/Student.vue'; // @ is an alias to /src
+
+  moment.locale('zh_tw');
+
+  @Options({
+    computed: {
+      diplayTime() {
+        return moment(this.currentTime).format('ll (dddd) a h:mm:ss');
+      }
+    },
+
+    methods: {
+      updateTime() {
+        this.currentTime = +new Date()
+        setTimeout(this.updateTime, 1000)
+      }
+    },
+
+
+    mounted() {
+      this.updateTime()
+    },
+
+    data() {
+      return {
+        currentTime: 0
+      }
+    },
+
+    components: {
+      Student,
+    },
+  })
+  export default class Home extends Vue {}
+</script>
