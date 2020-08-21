b73f3db4ab594b6dc9254a17f6f9ec5a1ba598b7
Jim Yeh <lemonlatte@gmail.com>
2020-08-21 11:54:29 +0800

    Adjust the style of login page and add the reset function

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,65 +1,67 @@
 <template>
   <div class="container">
-    <div class="row row justify-content-md-center">
-      <div class="col-6">
+    <div class="row justify-content-center">
+      <div class="col-md-5 col-sm-6 col-10">
         <form>
           <div class="form-group row">
-            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="staticEmail" v-model="email">
+            <label for="inputUsername" class="col-sm-3 col-4 col-form-label">帳號</label>
+            <div class="col-sm-9 col-8">
+              <input type="text" class="form-control" id="inputUsername" v-model="email" />
             </div>
           </div>
           <div class="form-group row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" id="inputPassword" v-model="password">
+            <label for="inputPassword" class="col-sm-3 col-4 col-form-label">密碼</label>
+            <div class="col-sm-9 col-8">
+              <input type="password" class="form-control" id="inputPassword" v-model="password" />
             </div>
           </div>
-          <button type="button" class="btn btn-primary" @click="login">Login</button>
+          <div class="form-group row justify-content-center">
+            <div class="btn-group col-md-8 col-sm-8 col-6" role="group">
+              <button type="button" class="btn btn-primary" @click="login">登入</button>
+              <button type="button" class="btn btn-secondary" @click="reset">清除</button>
+            </div>
+          </div>
         </form>
       </div>
     </div>
-
   </div>
 </template>
 
 <style scoped>
-  .container {
-    padding-top: 40px;
-  }
+.container {
+  padding-top: 40px;
+}
 </style>
 
 <script lang="ts">
-  import {
-    Options,
-    Vue
-  } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
 
-  import {
-    auth
-  } from '../firebase'
+import { auth } from "../firebase";
 
-  @Options({
-    methods: {
-      login() {
-        auth
-          .signInWithEmailAndPassword(this.email, this.password)
-          .then(() => {
-            this.$router.replace({
-              name: "Home"
-            });
-          })
-          .catch(err => {
-            alert(err.message)
+@Options({
+  methods: {
+    login() {
+      auth
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$router.replace({
+            name: "Home",
           });
-      }
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
     },
-    data() {
-      return {
-        email: "",
-        password: ""
-      }
-    }
-  })
-  export default class Login extends Vue {}
+    reset() {
+      (this.email = ""), (this.password = "");
+    },
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+})
+export default class Login extends Vue {}
 </script>
